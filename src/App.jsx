8e400646da7e1eae924cc982013deefc721e0534
Dragos Iordache <dragosh.iordache@yahoo.com>
2024-02-20 19:33:57 +0200

    733

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,24 @@ import { useEffect, useState } from 'react';
 
 function App() {
   const [name, setName] = useState('');
+  const [message, setMessage] = useState('');
 
   useEffect(() => {
-    axios.get('http://localhost:8080/profile').then((response) => {
-      const { data } = response;
+    axios
+      .get('http://localhost:8080/profile')
+      .then((response) => {
+        const { data } = response;
 
-      setName(data.name);
-    });
+        setName(data.name);
+      })
+      .catch((response) => {
+        const { code } = response;
+
+        if (code === 'ERR_NETWORK') {
+          setMessage('Could not fetch data');
+        }
+      });
   }, []);
-
   return (
     <h1 className="text-3xl font-bold underline">
       {name.trim().length <= 0 ? '...loading' : name}
