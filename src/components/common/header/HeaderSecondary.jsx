4d1a5120eb96c1b8e63b36138c943d846a0dd4b6
Dragos Iordache <dragosh.iordache@yahoo.com>
2024-02-27 20:32:16 +0200

    832

--- src/components/common/header/HeaderSecondary.jsx
+++ src/components/common/header/HeaderSecondary.jsx
@@ -1,7 +1,12 @@
+import { Button } from '../../ui';
+
 export const HeaderSecondary = () => {
   return (
     <section className="bg-gray-200 py-2 border-b border-b-gray-500">
       <div className="container mx-auto px-4">
+        <Button title="Add Contact" skin="success">
+          Add Contact
+        </Button>
         add contact daca e logat <br></br>
         login daca e nelogat (componenta)
       </div>
