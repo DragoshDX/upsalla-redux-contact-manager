6916f9d56479a18ac0fb17abc2fbe96c8b9f5ad0
Dragos Iordache <dragosh.iordache@yahoo.com>
2024-02-27 21:35:26 +0200

    935

--- src/components/common/header/HeaderSecondary.jsx
+++ src/components/common/header/HeaderSecondary.jsx
@@ -1,14 +1,21 @@
+import { useSelector } from 'react-redux';
 import { Button } from '../../ui';
 
 export const HeaderSecondary = () => {
+  const authenticated = useSelector(({ auth }) => {
+    return auth.authenticated;
+  });
+
   return (
     <section className="bg-gray-200 py-2 border-b border-b-gray-500">
       <div className="container mx-auto px-4">
-        <Button title="Add Contact" skin="success">
-          Add Contact
-        </Button>
-        add contact daca e logat <br></br>
-        login daca e nelogat (componenta)
+        {authenticated ? (
+          <Button title="Add Contact" skin="success">
+            Add Contact
+          </Button>
+        ) : (
+          <>google</>
+        )}
       </div>
     </section>
   );
