a2e337308e432742779f501d6a6b9686f92ce6a4
Dragos Iordache <dragosh.iordache@yahoo.com>
2024-02-29 21:18:32 +0200

    918

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,3 +1,7 @@
+import { jwtDecode } from 'jwt-decode';
+import store from './../store';
+import { setUser } from '../store/actions/auth';
+
 let initialized = false;
 const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
@@ -12,7 +16,25 @@ export const initializeGoogleAuth = async () => {
       google.accounts.id.initialize({
         client_id: clientId,
         callback: (response) => {
-          console.log(response);
+          const {
+            given_name: firstName,
+            family_name: lastName,
+            email,
+            picture: avatar,
+            sub: id,
+            name,
+          } = jwtDecode(response.credential);
+
+          store.dispatch(
+            setUser({
+              firstName,
+              lastName,
+              email,
+              avatar,
+              id,
+              name,
+            }),
+          );
         },
         scope: 'email profile',
       });
